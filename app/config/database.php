086d4f5f701c926970b9e7da5aad5531b89dc146
Daniil Tukmakov <fine.frizus@yandex.ru>
2024-07-11 11:58:08 +0500

    поправил конфиг `config/`, добавил параметр `app.debug`

--- app/config/database.php
+++ app/config/database.php
@@ -5,11 +5,11 @@ return [
     'connections' => [
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => getenv('POSTGRES_HOST') ?? '127.0.0.1',
-            'port' => getenv('POSTGRES_PORT') ?? '5432',
-            'database' => getenv('POSTGRES_DB') ?? 'db',
-            'username' => getenv('POSTGRES_USER') ?? null,
-            'password' => getenv('POSTGRES_PASSWORD') ?? null,
+            'host' => getenv('POSTGRES_HOST') ?: '127.0.0.1',
+            'port' => getenv('POSTGRES_PORT') ?: '5432',
+            'database' => getenv('POSTGRES_DB') ?: 'db',
+            'username' => getenv('POSTGRES_USER') ?: null,
+            'password' => getenv('POSTGRES_PASSWORD') ?: null,
             //'options' => [],
         ],
     ]
