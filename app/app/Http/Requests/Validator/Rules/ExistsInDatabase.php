734ec3d3a201a46ecc8338ae6dcec3056b351b27
Daniil Tukmakov <irfzus@gmail.com>
2024-07-11 16:41:24 +0500

    добавил правило `ExistsInDatabase` для валидатора

---
+++ app/app/Http/Requests/Validator/Rules/ExistsInDatabase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests\Validator\Rules;
+
+use Respect\Validation\Rules\AbstractRule;
+
+class ExistsInDatabase extends AbstractRule
+{
+    public function __construct(
+        protected string $tableName
+    )
+    {
+
+    }
+
+    public function validate($input): bool
+    {
+        return (bool)db()->table($this->tableName)->where('id', $input)->select('id')->fetch();
+    }
+}
