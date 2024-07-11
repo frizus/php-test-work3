f8e98dc3564b299ea900978ddfa495aeefdf1abf
Daniil Tukmakov <irfzus@gmail.com>
2024-07-11 17:32:00 +0500

    добавил реквест для экшна `show`

---
+++ app/app/Http/Requests/RestShowRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Respect\Validation\Validator;
+
+class RestShowRequest extends BaseRequest
+{
+    public function __construct(
+        protected string $tableName,
+        protected ?array $query = null,
+        protected ?array $post = null,
+        protected ?array $files = null
+    )
+    {
+        parent::__construct($query, $post, $files);
+    }
+
+    protected function callValidator(): void
+    {
+        Validator::key('id', Validator::intVal()->positive()->existsInDatabase($this->tableName))
+            ->assert($this->data);
+    }
+}
